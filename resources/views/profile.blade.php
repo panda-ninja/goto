69fab37ef16d02e21c4bca63814761762c506700
hidalgo <hidalgochpnce@gmail.com>
2016-10-25 14:49:38 -0500

    se cambio de la descripcion del blade profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet excepturi facere nostrum saepe? Aliquam blanditiis delectus enim excepturi expedita ipsam iure odio perferendis praesentium quisquam rerum sequi, sit tempora, tempore.
+        Este es un profile
 
     @stop
